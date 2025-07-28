185cb09eab1c7afa3d2b45617daca6a210bf1f05
Pedrosoxz <m.pinheirogv@gmail.com>
2025-07-28 15:24:56 -0300

    feat: Cria a funcionalidade divisão
    Recebe 2 valores de entrada de teclado e exibe o retorno da divisão deles.

---
+++ Calculadora/src/calculadora/Divisao.java
@@ -0,0 +1,19 @@
+package calculadora;
+
+
+import java.util.Scanner;
+
+
+public class Divisao {
+    public static void main(String[] args) {
+        Scanner entradaTeclado = new Scanner(System.in);
+        System.out.println("Informe o primeiro número:");
+        Integer numero1 = entradaTeclado.nextInt();
+        
+        System.out.println("Informe o segundo número:");
+        Integer numero2 = entradaTeclado.nextInt();
+        
+        System.out.println("A divisão dos números digitados é: " + (numero1/numero2));
+    }
+}
+
